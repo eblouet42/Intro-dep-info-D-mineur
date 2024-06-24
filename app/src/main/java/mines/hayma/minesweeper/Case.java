d80fee856160f858852f682a20578067152c75b5
Jafaar0 <h.hayma@orange.fr>
2024-06-24 11:09:58 +0200

    initialisation

--- app/src/main/java/mines/hayma/minesweeper/Case.java
+++ app/src/main/java/mines/hayma/minesweeper/Case.java
@@ -4,20 +4,13 @@ public class Case {
     public Boolean isMarked;
     public Boolean isClicked;
     public Boolean hasMine;
-
-    //coordonnées de la case
-    private int x;
-    private int y;
-    private Grille grille;
     int minesVoisines;
 
-    public Case(Boolean hasMine, int x, int y, Grille grille) {
-        this.hasMine = hasMine;
+    public Case() {
+        hasMine = false;
         isClicked = false;
         isMarked = false;
-        this.x = x;
-        this.y = y;
-        this.grille = grille;
+        minesVoisines = 0;
 
     }
 
@@ -27,20 +20,12 @@ public class Case {
 
     public void click(boolean caseCliquée){
         isClicked = true;
-        if (!caseCliquée) {
-            grille.click(x-1,y-1,false);
-            grille.click(x,y-1,false);
-            grille.click(x+1,y-1,false);
 
-            grille.click(x-1,y,false);
-            grille.click(x+1,y,false);
-
-            grille.click(x-1,y+1,false);
-            grille.click(x,y+1,false);
-            grille.click(x+1,y+1,false);
-        }
     }
 
+    public void setMinesVoisines(int n){
+        minesVoisines = n;
+    }
     public void mark() {
         isMarked = true;
     }
--- app/src/main/java/mines/hayma/minesweeper/Grille.java
+++ app/src/main/java/mines/hayma/minesweeper/Grille.java
@@ -1,25 +1,81 @@
 package mines.hayma.minesweeper;
 
+import java.util.Random;
 import java.util.Vector;
 
 public class Grille {
-    int nbCase;
-    Vector<Case> Cases;
+    int lignes;
+    int colonnes;
+    int nbMines
+    Case[][] Cases;
 
-    public Grille(){
+    public Grille(int lignes, int colonnes, int nbMines){
+        this.lignes = lignes;
+        this.colonnes = colonnes;
+        this.nbMines = nbMines;
+        Cases = new Case[lignes][colonnes];
+        initialisation();
 
     }
 
-    public void click(int x, int y, Boolean caseCliquée) {
-        int num = toNum(x,y);
-        Cases.get(num).click(caseCliquée);
+    private void initialisation() {
+        for (int i=0; i<lignes; i++){
+            for (int j=0; j<colonnes; j++){
+                Cases[i][j] = new Case();
+
+            }
+        }
+        placerMines();
+        calculervoisins();
     }
 
-    public int toNum(int x, int y) {
-        return y*nbCase + x;
+    private Boolean estDansLaGrille(int x, int y){
+        return x >= 0 && x < lignes && y >= 0 && y < colonnes;
     }
-    public int[] toCoord(int num) {
-        return new int[]{num%nbCase, num/nbCase};
+
+    private void calculervoisins() {
+        for (int i=0; i<lignes; i++){
+            for (int j=0; j<colonnes; j++){
+                int compteur = 0;
+                for (int di=-1; di<2; di++){
+                    for (int dj=-1; dj<2; dj++){
+                        if estDansLaGrille(i+di,j+dj) {
+                            compteur++;
+                        }
+                    }
+                }
+                Cases[i][j].setMinesVoisines(compteur);
+
+            }
+        }
     }
+
+    private void placerMines() {
+        Random random = new Random();
+        int minesPlacées = 0;
+        while (minesPlacées<nbMines){
+            int ligne = random.nextInt(lignes);
+            int colonne = random.nextInt(colonnes);
+            if (!Cases[ligne][colonne].hasMine) {
+                Cases[ligne][colonne].setMine(true);
+                minesPlacées++;
+            }
+        }
+    }
+
+    public void click(int x, int y,) {
+        Cases[x][y].click();
+        if (Cases[x][y].minesVoisines == 0) {//clique les voisins si pas de mines autour
+            for (int dx=-1; dx<2; dx++){
+                for (int dy=-1; dy<2; dy++){
+                    if estDansLaGrille(x+dx,y+dy) {
+                        click(x+dx,y+dy);
+                    }
+                }
+            }
+        }
+    }
+
+
 
 }
