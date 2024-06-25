476be4318fc92cedd328c66abf74595a0b4995de
Jafaar0 <h.hayma@orange.fr>
2024-06-25 11:35:04 +0200

    edit

--- app/src/main/java/mines/hayma/minesweeper/Case.java
+++ app/src/main/java/mines/hayma/minesweeper/Case.java
@@ -1,12 +1,8 @@
 package mines.hayma.minesweeper;
 
-import android.annotation.SuppressLint;
-import android.content.Context;
-
 public class Case {
     public Boolean isMarked;
     public Boolean isClicked;
-    public Boolean isDiscoqueen;
     public Boolean hasMine;
     int minesVoisines;
 
@@ -14,7 +10,6 @@ public class Case {
         hasMine = false;
         isClicked = false;
         isMarked = false;
-        isDiscoqueen = false;
         minesVoisines = 0;
     }
 
@@ -22,33 +17,20 @@ public class Case {
         hasMine = bool;
     }
 
-    public void click(){
+    public void click() {
         isClicked = true;
-        isDiscoqueen=true;
+
     }
 
-    public void reversedrapo(){
-        isMarked=!isMarked;
-    }
-
-    public void setMinesVoisines(int n){
+    public void setMinesVoisines(int n) {
         minesVoisines = n;
     }
+
+    public void mark() {
+        isMarked = !isMarked;
+    }
+
     public int getMinesVoisines() {
         return minesVoisines;
-    }
-    @SuppressLint("DiscouragedApi")
-    public int getImageResource(Context context) {
-        if (isDiscoqueen) {
-            if (hasMine) {
-                return R.drawable.bomb;
-            } else {
-                return context.getResources().getIdentifier("number" + getMinesVoisines(), "drawable", context.getPackageName());
-            }
-        } else if (isMarked) {
-            return R.drawable.flag;
-        } else {
-            return R.drawable.untouched;
-        }
     }
 }
